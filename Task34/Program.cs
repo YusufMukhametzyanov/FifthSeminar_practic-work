80a68f11c3bab7ffc42cbe2ac7d078e135af6d01
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-16 08:23:29 +0500

    Add 34 & 36 tasks

---
+++ Task34/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+//инициализация массива
+int[] GetArray(int arraySize)
+{
+    int[] arr = new int[arraySize];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+//вывод на печать массива
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+//определение количества чётных чисел
+void GetCountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    Console.WriteLine($"в массиве {count} чётных чисел(а)");
+}
+
+Console.Write("Введите размерность массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] arr = GetArray(arraySize);
+PrintArray(arr);
+GetCountEvenNumbers(arr);
---
+++ Task36/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] GetArray(int arraySize)
+{
+    int[] arr = new int[arraySize];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-10, 10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+}
+
+void GetSumOddPosition(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+            sum += arr[i];
+    }
+    Console.WriteLine($" -> {sum}");
+}
+
+Console.Write("Введите размер массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] arr = GetArray(arraySize);
+PrintArray(arr);
+GetSumOddPosition(arr);
+
+
+
+
+
