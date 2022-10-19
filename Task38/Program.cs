4949d24c1e374c906d8c30149e22b49679eb0a77
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-19 10:38:08 +0500

    Add practic work for fifth seminar

---
+++ Task38/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+int[] GetArray(int array)
+{
+    int[] arr = new int[array];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-10, 10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+}
+
+void GetSumOddPosition(int[] arr)
+{
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= max) max = arr[i];
+        else if (arr[i] <= min) min = arr[i];
+    }
+    int result = max - min;
+    Console.WriteLine($" Разница между максимальным и минимальным элементом массива = {result}");
+}
+
+Console.Write("Введите размер массива: ");
+int array = int.Parse(Console.ReadLine());
+int[] arr = GetArray(array);
+PrintArray(arr);
+GetSumOddPosition(arr);
